791d8b60f9582d3ded4a0376c5401d47a9b298d9
Nick Aja <ajanick3@gmail.com>
2020-01-30 10:57:18 -0500

    rm comments

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MaterialTable, { Column } from 'material-table'
 
-interface Row {
+interface RowData {
   name: string
   surname: string
   birthYear: number
@@ -9,14 +9,14 @@ interface Row {
 }
 
 interface Props {
-  columns: Column<Row>[]
-  data: Row[]
+  columns: Column<RowData>[]
+  data: RowData[]
 }
 
 export default function Table(props: Props) {
   const { columns, data } = props
   if (columns.length !== Object.keys(data[0]).length) {
-    console.log('not valid')
+    console.log('not valid', columns.length, Object.keys(data[0]).length)
     return <div>fail</div>
   }
   return (
@@ -32,32 +32,3 @@ export default function Table(props: Props) {
     />
   )
 }
-
-// export default function MaterialTableDemo() {
-//   // const columns = [
-//   //   { title: "Name", field: "name" },
-//   //   { title: "Surname", field: "surname" },
-//   //   { title: "Birth Year", field: "birthYear", type: "numeric" },
-//   //   {
-//   //     title: "Birth Place",
-//   //     field: "birthCity",
-//   //     lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-//   //   }
-//   // ];
-
-//   // const data = [
-//   //   { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-//   //   {
-//   //     name: "Zerya Betül",
-//   //     surname: "Baran",
-//   //     birthYear: 2017,
-//   //     birthCity: 34
-//   //   }
-//   // ];
-
-//   return (
-//     <>
-//       <Table columns={columns} data={data} />
-//     </>
-//   );
-// }
