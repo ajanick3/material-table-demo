a7d8efb817564932831968bdb3b000c65168d000
Nick Aja <ajanick3@gmail.com>
2020-01-30 11:47:13 -0500

    mock data

--- src/Table.tsx
+++ src/Table.tsx
@@ -2,33 +2,35 @@ import React from 'react'
 import MaterialTable, { Column } from 'material-table'
 
 interface RowData {
-  name: string
-  surname: string
-  birthYear: number
-  birthCity: number
+  [key: string]: string | number
 }
 
 interface Props {
+  title?: string
   columns: Column<RowData>[]
   data: RowData[]
 }
 
 export default function Table(props: Props) {
-  const { columns, data } = props
-  if (columns.length !== Object.keys(data[0]).length) {
-    console.log('not valid', columns.length, Object.keys(data[0]).length)
-    return <div>fail</div>
+  const {
+    title = 'Table',
+    columns,
+    data,
+    //
+  } = props
+  if (data.length && Object.keys(data[0]).length === columns.length) {
+    return (
+      <MaterialTable
+        title={title}
+        columns={columns}
+        data={data}
+        options={{
+          sorting: true,
+          // search: true,
+          // filtering: true
+        }}
+      />
+    )
   }
-  return (
-    <MaterialTable
-      title="Editable Example"
-      columns={columns}
-      data={data}
-      options={{
-        sorting: true,
-        // search: true,
-        // filtering: true
-      }}
-    />
-  )
+  return null
 }
